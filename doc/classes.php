0579a613f04531fd2fb8444d708b93f54f9ee56e
Corry Frydlewicz <cfrydlewicz@gmail.com>
2017-09-08 12:31:48 -0700

    Begin Documenting Identification Classes

---
+++ doc/classes.php
@@ -0,0 +1,15 @@
+<?php include 'inc/doc_head-min.php'; ?>
+
+<section>
+	<h2>Identification Classes</h2>
+	<p>These classes do not apply any styles on their own. They are used to identify element qualities that may alter their children's styling.</p>
+
+	<h3>i_copy-area</h3>
+	<p>Identifies an area where the text should be interpretted as longform.</p>
+
+	<h3>i_dark-bg</h3>
+	<p>Identifies an element with a dark background color.</p>
+	
+</section>
+
+<?php include 'inc/doc_foot-min.php'; ?>
